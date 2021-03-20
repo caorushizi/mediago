3f3efdace80c099b44c4d5c357afbc58edb7d8f4
caorushizi <84996057@qq.com>
2021-03-20 16:10:39 +0800

    feat: vite2

---
+++ main/type.d.ts
@@ -0,0 +1,7 @@
+declare global {
+  interface Global {
+    __bin__: string;
+  }
+}
+
+export {};
--- scripts/build.bat
+++
@@ -1,5 +0,0 @@
-chcp 65001
-
-rd /S /Q "./.bin/Logs"
-
-npx cross-env NODE_ENV=production electron-forge make
