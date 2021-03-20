de4b5e98bf34847a2dd1d0daa997811afeb80ba0
caorushizi <84996057@qq.com>
2021-03-20 20:28:38 +0800

    feat: add env files.

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,5 +1,4 @@
 const { resolve } = require("path");
-console.log(__dirname);
 
 require("dotenv").config({
   path: resolve(__dirname, `.env.${process.env.NODE_ENV}`),
@@ -12,6 +11,9 @@ require("esbuild").buildSync({
   sourcemap: true,
   target: ["node10.4"],
   external: ["electron"],
-  define: {},
+  define: {
+    // 开发环境中二进制可执行文件的路径
+    __bin__: `"${resolve(__dirname, ".bin").replace(/\\/g, "\\\\")}"`,
+  },
   outdir: resolve(__dirname, "./dist/"),
 });
