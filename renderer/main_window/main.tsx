43a586ed892f2cf1c4291d7fb9bc98e0daa5af99
caorushizi <84996057@qq.com>
2021-03-20 17:12:44 +0800

    feat: vite2

--- renderer/main_window/main.tsx
+++ renderer/main_window/main.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom";
-import React from "react";
+import * as ReactDOM from "react-dom";
+import * as React from "react";
 import "./index.scss";
 import App from "./App";
 import tdApp from "../renderer_common/td";
