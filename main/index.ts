d512d78291f9429130659e915e9c967b0a13bd46
caorushizi <84996057@qq.com>
2021-03-21 20:20:28 +0800

    feat: ♥ 生产版本构建

--- main/index.ts
+++ main/index.ts
@@ -1,6 +1,6 @@
-import { app, BrowserView, BrowserWindow, ipcMain, session } from "electron";
+import { app, BrowserWindow, protocol, session } from "electron";
 import { is } from "electron-util";
-import path from "path";
+import { resolve } from "path";
 import logger from "./utils/logger";
 import windowManager from "./window/windowManager";
 import { WindowName } from "./window/variables";
@@ -12,10 +12,12 @@ if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
+// protocol.registerSchemesAsPrivileged([
+//   { scheme: "mediago", privileges: { bypassCSP: true } },
+// ]);
+
 if (!is.development) {
-  global.__bin__ = path
-    .resolve(app.getAppPath(), "../.bin")
-    .replace(/\\/g, "\\\\");
+  global.__bin__ = resolve(app.getAppPath(), "../.bin").replace(/\\/g, "\\\\");
 }
 
 const init = async () => {
@@ -37,13 +39,20 @@ app.on("activate", async () => {
 });
 
 app.whenReady().then(async () => {
+  protocol.registerFileProtocol("mediago", (request, callback) => {
+    const url = request.url.substr(10);
+    console.log("url: ", url);
+    console.log("after: ", resolve(__dirname, "../electron", url));
+    callback({ path: resolve(__dirname, "../electron", url) });
+  });
+
   await init();
 
   if (is.development) {
     try {
-      const reactTool = path.resolve(__dirname, "../devtools/react");
+      const reactTool = resolve(__dirname, "../../devtools/react");
       await session.defaultSession.loadExtension(reactTool);
-      const reduxTool = path.resolve(__dirname, "../devtools/redux");
+      const reduxTool = resolve(__dirname, "../../devtools/redux");
       await session.defaultSession.loadExtension(reduxTool);
     } catch (e) {
       logger.info(e);
--- renderer/renderer_common/td.ts
+++ renderer/renderer_common/td.ts
@@ -2,6 +2,7 @@ const onEvent = (eventId: string, mapKv: any = {}) => {
   try {
     // @ts-ignore
     window.TDAPP.onEvent(eventId, "", mapKv);
+    console.log("添加埋点成功：", eventId, mapKv);
   } catch (err) {
     console.log("添加埋点失败：", err);
   }
@@ -12,7 +13,9 @@ const init = () => {
   const vn = import.meta.env.VITE_APP_TDVN;
   const vc = import.meta.env.VITE_APP_TDVC;
   const script = document.createElement("script");
-  script.src = `https://jic.talkingdata.com/app/h5/v1?appid=${appId}&vn=${vn}&vc=${vc}`;
+  const src = `https://jic.talkingdata.com/app/h5/v1?appid=${appId}&vn=${vn}&vc=${vc}`;
+  script.src = src;
+  console.log("埋点地址是：", src);
   const headElement = document.getElementsByTagName("head")[0];
   headElement.appendChild(script);
 };
