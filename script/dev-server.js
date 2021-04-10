62a28d6d11d1e52217769cba6956daea2c96af76
caorushizi <84996057@qq.com>
2021-04-11 03:46:27 +0800

    feat: 🎞 重新安装 eslint

---
+++ script/dev-server.js
@@ -0,0 +1,30 @@
+const concurrently = require("concurrently");
+
+concurrently(
+  [
+    {
+      command: "vite",
+      name: "vite",
+      prefixColor: "green",
+      env: { NODE_ENV: "development" },
+    },
+    {
+      command: "node esbuild.config.js && electron dist/main/index.js",
+      name: "electron",
+      prefixColor: "blue",
+      env: { NODE_ENV: "development" },
+    },
+  ],
+  {
+    prefix: "name",
+    killOthers: ["failure", "success"],
+    restartTries: 3,
+  }
+).then(
+  () => {
+    // 成功
+  },
+  () => {
+    // 失败
+  }
+);
