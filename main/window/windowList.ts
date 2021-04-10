f58bd9c1901f6f9e87c6677a933e3f926538f651
caorushizi <84996057@qq.com>
2021-04-10 15:41:51 +0800

    feat: 🥳 恢复原先的布局

--- main/window/windowList.ts
+++ main/window/windowList.ts
@@ -54,49 +54,49 @@ windowList.set(Windows.BROWSER_WINDOW, {
   },
 });
 
-windowList.set(Windows.SETTING_WINDOW, {
-  url: is.development
-    ? "http://localhost:3000/setting-window.html"
-    : "mediago://electron/settin-window.html",
-  options(): Electron.BrowserWindowConstructorOptions {
-    return {
-      width: 600,
-      height: 400,
-      show: false,
-      frame: false,
-      webPreferences: {
-        nodeIntegration: false,
-        contextIsolation: true,
-        enableRemoteModule: false,
-        preload: resolve(__dirname, "./preload.js"),
-      },
-    };
-  },
-  async callback(window) {
-    if (is.development) window.webContents.openDevTools();
-  },
-});
+// windowList.set(Windows.SETTING_WINDOW, {
+//   url: is.development
+//     ? "http://localhost:3000/setting-window.html"
+//     : "mediago://electron/settin-window.html",
+//   options(): Electron.BrowserWindowConstructorOptions {
+//     return {
+//       width: 600,
+//       height: 400,
+//       show: false,
+//       frame: false,
+//       webPreferences: {
+//         nodeIntegration: false,
+//         contextIsolation: true,
+//         enableRemoteModule: false,
+//         preload: resolve(__dirname, "./preload.js"),
+//       },
+//     };
+//   },
+//   async callback(window) {
+//     if (is.development) window.webContents.openDevTools();
+//   },
+// });
 
-windowList.set(Windows.CONFIRM_WINDOW, {
-  url: is.development
-    ? "http://localhost:3000/confirm-window.html"
-    : "mediago://electron/confirm-window.html",
-  options(): Electron.BrowserWindowConstructorOptions {
-    return {
-      width: 300,
-      height: 200,
-      show: false,
-      frame: false,
-      webPreferences: {
-        nodeIntegration: false,
-        contextIsolation: true,
-        enableRemoteModule: false,
-      },
-    };
-  },
-  async callback(window) {
-    // if (is.development) window.webContents.openDevTools();
-  },
-});
+// windowList.set(Windows.CONFIRM_WINDOW, {
+//   url: is.development
+//     ? "http://localhost:3000/confirm-window.html"
+//     : "mediago://electron/confirm-window.html",
+//   options(): Electron.BrowserWindowConstructorOptions {
+//     return {
+//       width: 300,
+//       height: 200,
+//       show: false,
+//       frame: false,
+//       webPreferences: {
+//         nodeIntegration: false,
+//         contextIsolation: true,
+//         enableRemoteModule: false,
+//       },
+//     };
+//   },
+//   async callback(window) {
+//     // if (is.development) window.webContents.openDevTools();
+//   },
+// });
 
 export default windowList;
