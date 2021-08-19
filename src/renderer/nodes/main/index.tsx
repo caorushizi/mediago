258ff58f1ac5782d0ba0e54cea578fbc3ac87eaa
caorushizi <84996057@qq.com>
2021-08-19 22:43:27 +0800

    fix: 删除测试代码。

--- src/renderer/nodes/main/index.tsx
+++ src/renderer/nodes/main/index.tsx
@@ -23,7 +23,6 @@ import { updateSettings } from "renderer/store/actions/settings.actions";
 import { AppState } from "renderer/store/reducers";
 import { Settings } from "renderer/store/models/settings";
 import electron from "renderer/utils/electron";
-import { request } from "renderer/utils";
 
 const audio = new Audio(audioSrc);
 
@@ -94,16 +93,6 @@ class MainPage extends React.Component<PropsFromRedux, State> {
     });
 
     electron.addEventListener("m3u8", this.handleWebViewMessage);
-
-    const resp = await request({
-      method: "post",
-      url: "https://baidu.com",
-      data: {
-        username: 123,
-        password: 123,
-      },
-    });
-    console.log("resp", resp);
   }
 
   componentWillUnmount(): void {
--- src/types/renderer.d.ts
+++ src/types/renderer.d.ts
@@ -135,3 +135,70 @@ declare interface AppStore {
   useProxy: boolean;
   exeFile: string;
 }
+
+declare interface Manifest {
+  allowCache: boolean;
+  endList: boolean;
+  mediaSequence: number;
+  discontinuitySequence: number;
+  playlistType: string;
+  custom: Record<string, unknown>;
+  playlists: [
+    {
+      attributes: Record<string, unknown>;
+      Manifest: Manifest;
+    }
+  ];
+  mediaGroups: {
+    AUDIO: {
+      "GROUP-ID": {
+        NAME: {
+          default: boolean;
+          autoselect: boolean;
+          language: string;
+          uri: string;
+          instreamId: string;
+          characteristics: string;
+          forced: boolean;
+        };
+      };
+    };
+    VIDEO: Record<string, unknown>;
+    "CLOSED-CAPTIONS": Record<string, unknown>;
+    SUBTITLES: Record<string, unknown>;
+  };
+  dateTimeString: string;
+  dateTimeObject: Date;
+  targetDuration: number;
+  totalDuration: number;
+  discontinuityStarts: [number];
+  segments: [
+    {
+      byterange: {
+        length: number;
+        offset: number;
+      };
+      duration: number;
+      attributes: Record<string, unknown>;
+      discontinuity: number;
+      uri: string;
+      timeline: number;
+      key: {
+        method: string;
+        uri: string;
+        iv: string;
+      };
+      map: {
+        uri: string;
+        byterange: {
+          length: number;
+          offset: number;
+        };
+      };
+      "cue-out": string;
+      "cue-out-cont": string;
+      "cue-in": string;
+      custom: Record<string, unknown>;
+    }
+  ];
+}
