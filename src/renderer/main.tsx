51023a2feb5a2e6c6ea13ff38439e9865617b9be
caorushizi <84996057@qq.com>
2021-08-23 23:12:27 +0800

    fix: build script.

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import { HashRouter } from "react-router-dom";
-import { Route, Switch, Redirect } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router";
 import tdApp from "./utils/td";
 import "antd/dist/antd.css";
 import { Provider } from "react-redux";
@@ -14,13 +14,10 @@ tdApp.init();
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
-      <Switch>
-        <Route path="/main" component={MainPage} />
-        <Route path="/browser" component={BrowserPage} />
-        <Redirect to={"/main"} />
-      </Switch>
-    </HashRouter>
+    <BrowserRouter>
+      <Route path="/main" component={MainPage} />
+      <Route path="/browser" component={BrowserPage} />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
