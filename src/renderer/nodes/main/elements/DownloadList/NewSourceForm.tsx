c6abe997014703b9109c6b232511a324db9b54ec
caorushizi <84996057@qq.com>
2021-12-05 03:20:28 +0800

    fix: react hooks.

--- src/renderer/nodes/main/elements/DownloadList/NewSourceForm.tsx
+++ src/renderer/nodes/main/elements/DownloadList/NewSourceForm.tsx
@@ -1,11 +1,9 @@
-import React, { ReactNode } from "react";
+import React, { FC, useRef } from "react";
 import { Button, Form, FormInstance, Input, Modal, Switch } from "antd";
 import { SourceItemForm } from "types/common";
-import { connect, ConnectedProps } from "react-redux";
+import { useSelector } from "react-redux";
 import { AppState } from "renderer/store/reducers";
-import { Dispatch } from "redux";
 import { Settings } from "renderer/store/models/settings";
-import { updateSettings } from "renderer/store/actions/settings.actions";
 
 interface Props {
   visible: boolean;
@@ -18,35 +16,37 @@ const headersPlaceholder = `[可空] 请输入一行一个Header，例如：
 Origin: https://www.sample.com
 Referer: https://www.sample.com`;
 
-class NewSourceForm extends React.Component<PropsFromRedux> {
-  formRef = React.createRef<FormInstance<SourceItemForm>>();
+const NewSourceForm: FC<Props> = ({
+  handleOk,
+  handleDownload,
+  handleCancel,
+  visible,
+}) => {
+  const formRef = useRef<FormInstance<SourceItemForm>>();
+  const settings = useSelector<AppState, Settings>((state) => state.settings);
+
+  const { exeFile } = settings;
 
   // 点击确定按钮
-  handleOk = async (): Promise<void> => {
-    const { handleOk } = this.props;
-
-    const formRef = this.formRef.current;
-    if (formRef && (await formRef.validateFields())) {
-      const item = formRef.getFieldsValue();
+  const handleConfirm = async (): Promise<void> => {
+    if (formRef.current && (await formRef.current.validateFields())) {
+      const item = formRef.current.getFieldsValue();
       await handleOk(item);
-      formRef.resetFields();
+      formRef.current.resetFields();
     }
   };
 
   // 点击确定按钮
-  handleDownload = async (): Promise<void> => {
-    const { handleDownload } = this.props;
-
-    const formRef = this.formRef.current;
-    if (formRef && (await formRef.validateFields())) {
-      const item = formRef.getFieldsValue();
+  const handleDownload1 = async (): Promise<void> => {
+    if (formRef.current && (await formRef.current.validateFields())) {
+      const item = formRef.current.getFieldsValue();
       await handleDownload(item);
-      formRef.resetFields();
+      formRef.current.resetFields();
     }
   };
 
   // 文件拖入事件
-  handleFileDrop = (e: React.DragEvent<HTMLInputElement>): void => {
+  const handleFileDrop = (e: React.DragEvent<HTMLInputElement>): void => {
     e.preventDefault();
 
     const dataTransfer = e.dataTransfer;
@@ -58,77 +58,59 @@ class NewSourceForm extends React.Component<PropsFromRedux> {
       return;
     }
     const path = dataTransfer.files[0].path;
-    this.formRef.current?.setFieldsValue({ url: path });
+    formRef.current?.setFieldsValue({ url: path });
   };
 
-  render(): ReactNode {
-    const { visible, handleCancel, settings } = this.props;
-    const { exeFile } = settings;
-    return (
-      <Modal
-        title="新建下载"
-        visible={visible}
-        onCancel={handleCancel}
-        footer={[
-          <Button key="back" onClick={this.handleDownload}>
-            立即下载
-          </Button>,
-          <Button key="submit" onClick={handleCancel}>
-            取消
-          </Button>,
-          <Button key="link" type="primary" onClick={this.handleOk}>
-            添加
-          </Button>,
-        ]}
+  return (
+    <Modal
+      title="新建下载"
+      visible={visible}
+      onCancel={handleCancel}
+      footer={[
+        <Button key="back" onClick={handleDownload1}>
+          立即下载
+        </Button>,
+        <Button key="submit" onClick={handleCancel}>
+          取消
+        </Button>,
+        <Button key="link" type="primary" onClick={handleConfirm}>
+          添加
+        </Button>,
+      ]}
+    >
+      <Form
+        labelCol={{ span: 4 }}
+        ref={formRef}
+        initialValues={{ delete: true }}
       >
-        <Form
-          labelCol={{ span: 4 }}
-          ref={this.formRef}
-          initialValues={{ delete: true }}
+        <Form.Item
+          label="m3u8"
+          name="url"
+          rules={[{ required: true, message: "请填写 m3u8 链接" }]}
         >
-          <Form.Item
-            label="m3u8"
-            name="url"
-            rules={[{ required: true, message: "请填写 m3u8 链接" }]}
-          >
-            <Input placeholder="[必填] 输入 m3u8 地址，或将M3U8文件拖拽至此" />
-          </Form.Item>
-          <Form.Item
-            label="视频名称"
-            name="title"
-            rules={[{ required: true, message: "请填写视频名称" }]}
-          >
-            <Input placeholder="[可空] 默认当前时间戳" />
-          </Form.Item>
-          <Form.Item label="请求标头" name="headers">
-            <Input.TextArea rows={3} placeholder={headersPlaceholder} />
-          </Form.Item>
-          <Form.Item
-            label="下载完成是否删除"
-            name="delete"
-            labelCol={{ span: 8 }}
-            valuePropName="checked"
-          >
-            <Switch disabled={exeFile === "mediago"} />
-          </Form.Item>
-        </Form>
-      </Modal>
-    );
-  }
-}
-
-const mapStateToProps = (state: AppState) => ({
-  settings: state.settings.settings,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    setSettings: (settings: Partial<Settings>) =>
-      dispatch(updateSettings(settings)),
-  };
+          <Input placeholder="[必填] 输入 m3u8 地址，或将M3U8文件拖拽至此" />
+        </Form.Item>
+        <Form.Item
+          label="视频名称"
+          name="title"
+          rules={[{ required: true, message: "请填写视频名称" }]}
+        >
+          <Input placeholder="[可空] 默认当前时间戳" />
+        </Form.Item>
+        <Form.Item label="请求标头" name="headers">
+          <Input.TextArea rows={3} placeholder={headersPlaceholder} />
+        </Form.Item>
+        <Form.Item
+          label="下载完成是否删除"
+          name="delete"
+          labelCol={{ span: 8 }}
+          valuePropName="checked"
+        >
+          <Switch disabled={exeFile === "mediago"} />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
 };
 
-const connector = connect(mapStateToProps, mapDispatchToProps);
-type PropsFromRedux = ConnectedProps<typeof connector> & Props;
-
-export default connector(NewSourceForm);
+export default NewSourceForm;
--- src/renderer/nodes/main/elements/Setting/index.tsx
+++ src/renderer/nodes/main/elements/Setting/index.tsx
@@ -1,14 +1,6 @@
-import React, { ReactNode } from "react";
-import {
-  Button,
-  Descriptions,
-  FormInstance,
-  Space,
-  Switch,
-  Tooltip,
-} from "antd";
+import React, { FC, useEffect, useRef, useState } from "react";
+import { Button, FormInstance, Space, Switch, Tooltip } from "antd";
 import "./index.scss";
-import variables from "renderer/utils/variables";
 import ProForm, {
   ProFormGroup,
   ProFormSelect,
@@ -17,19 +9,12 @@ import ProForm, {
 } from "@ant-design/pro-form";
 import { FolderOpenOutlined } from "@ant-design/icons";
 import { AppState } from "renderer/store/reducers";
-import { Dispatch } from "redux";
-import { Settings } from "renderer/store/models/settings";
 import { updateSettings } from "renderer/store/actions/settings.actions";
-import { connect, ConnectedProps } from "react-redux";
-
-interface Downloader {
-  title: string;
-  description: string;
-  github: string;
-}
+import { useDispatch, useSelector } from "react-redux";
+import { Box } from "@chakra-ui/react";
+import { Settings } from "renderer/store/models/settings";
 
 interface State {
-  downloader: Downloader;
   proxyChecked: boolean;
 }
 
@@ -55,39 +40,20 @@ if (window.electron.is.windows) {
 }
 
 // 设置页面
-class Setting extends React.Component<PropsFromRedux, State> {
-  formRef = React.createRef<FormInstance<FormData>>();
+const Setting: FC = () => {
+  const settings = useSelector<AppState, Settings>((state) => state.settings);
+  const dispatch = useDispatch();
+  const formRef = useRef<FormInstance<FormData>>();
+  const [proxyChecked, setProxyChecked] = useState<boolean>(false);
+  console.log("settings: ", settings);
 
-  constructor(props: PropsFromRedux) {
-    super(props);
-    this.state = {
-      proxyChecked: false,
-      downloader: {
-        title: "",
-        description: "",
-        github: "",
-      },
-    };
-  }
-
-  async componentDidMount(): Promise<void> {
-    const { settings } = this.props;
-    const { exeFile, useProxy } = settings;
-    this.setState({
-      proxyChecked: useProxy,
-      downloader: {
-        title: exeFile === "mediago" ? "mediago" : "N_m3u8DL-CLI",
-        description: "",
-        github:
-          exeFile === "mediago"
-            ? variables.urls.mediaGoUrl
-            : variables.urls.m3u8Url,
-      },
-    });
-  }
+  useEffect(() => {
+    const { useProxy } = settings;
+    setProxyChecked(useProxy);
+  }, []);
 
   // 选择下载地址
-  handleSelectDir = async (): Promise<void> => {
+  const handleSelectDir = async (): Promise<void> => {
     const defaultPath = await window.electron.getPath("documents");
     const { filePaths } = await window.electron.showOpenDialog({
       defaultPath,
@@ -97,165 +63,123 @@ class Setting extends React.Component<PropsFromRedux, State> {
     if (!filePaths) return;
     // 返回值为空
     if (Array.isArray(filePaths) && filePaths.length <= 0) return;
-    const { setSettings } = this.props;
     const workspaceValue = filePaths[0];
     await window.electron.store.set("workspace", workspaceValue);
-    this.formRef.current?.setFieldsValue({
+    formRef.current?.setFieldsValue({
       workspace: workspaceValue || "",
     });
-    setSettings({ workspace: workspaceValue });
+
+    dispatch(updateSettings({ workspace: workspaceValue }));
   };
 
   // 打开配置文件文件夹
-  openConfigDir = async (): Promise<void> => {
+  const openConfigDir = async (): Promise<void> => {
     window.electron.openConfigDir();
   };
 
   // 打开可执行程序文件夹
-  openBinDir = () => {
+  const openBinDir = () => {
     window.electron.openBinDir();
   };
 
   // 本地存储文件夹
-  localDir = async (): Promise<void> => {
-    const { settings } = this.props;
+  const localDir = async (): Promise<void> => {
     const { workspace } = settings;
     window.electron.openPath(workspace);
   };
 
   // 更改代理设置
-  toggleProxySetting = async (enableProxy: boolean): Promise<void> => {
-    this.setState({ proxyChecked: enableProxy });
+  const toggleProxySetting = async (enableProxy: boolean): Promise<void> => {
+    setProxyChecked(enableProxy);
     await window.electron.store.set("useProxy", enableProxy);
   };
 
-  render(): ReactNode {
-    const { downloader, proxyChecked } = this.state;
-    const { settings } = this.props;
-    const { workspace, exeFile, tip, proxy } = settings;
+  const { workspace, exeFile, tip, proxy } = settings;
 
-    return (
-      <div className="setting-form">
-        <ProForm<FormData>
-          formRef={this.formRef as any}
-          layout="horizontal"
-          submitter={false}
-          initialValues={{ workspace, exeFile, tip, proxy }}
-          onValuesChange={async (changedValue) => {
-            if (Object.keys(changedValue).includes("tip")) {
-              await window.electron.store.set("tip", changedValue["tip"]);
+  return (
+    <Box className="setting-form">
+      <ProForm<FormData>
+        formRef={formRef}
+        layout="horizontal"
+        submitter={false}
+        initialValues={{ workspace, exeFile, tip, proxy }}
+        onValuesChange={async (changedValue) => {
+          if (Object.keys(changedValue).includes("tip")) {
+            await window.electron.store.set("tip", changedValue["tip"]);
+          }
+          if (Object.keys(changedValue).includes("exeFile")) {
+            const value = changedValue["exeFile"];
+            await window.electron.store.set("exeFile", value);
+            dispatch(updateSettings({ exeFile: value }));
+          }
+          // 代理 onchange 事件
+          if (Object.keys(changedValue).includes("proxy")) {
+            const value = changedValue["proxy"];
+            await window.electron.store.set("proxy", value);
+            if (proxyChecked) {
+              await toggleProxySetting(false);
             }
-            if (Object.keys(changedValue).includes("exeFile")) {
-              const { setSettings } = this.props;
-              const value = changedValue["exeFile"];
-              await window.electron.store.set("exeFile", value);
-              setSettings({ exeFile: value });
-              this.setState({
-                downloader: {
-                  title: value === "mediago" ? "mediago" : "N_m3u8DL-CLI",
-                  description: "",
-                  github:
-                    value === "mediago"
-                      ? variables.urls.mediaGoUrl
-                      : variables.urls.m3u8Url,
-                },
-              });
+          }
+        }}
+      >
+        <ProFormGroup label="基础设置">
+          <ProFormText
+            width="xl"
+            disabled
+            name="workspace"
+            placeholder="请选择视频下载目录"
+            label={
+              <Button onClick={handleSelectDir} icon={<FolderOpenOutlined />}>
+                选择文件夹
+              </Button>
             }
-            // 代理 onchange 事件
-            if (Object.keys(changedValue).includes("proxy")) {
-              const value = changedValue["proxy"];
-              await window.electron.store.set("proxy", value);
-              if (this.state.proxyChecked) {
-                await this.toggleProxySetting(false);
-              }
-            }
-          }}
-        >
-          <ProFormGroup label="基础设置">
-            <ProFormText
-              width="xl"
-              disabled
-              name="workspace"
-              placeholder="请选择视频下载目录"
-              label={
-                <Button
-                  onClick={this.handleSelectDir}
-                  icon={<FolderOpenOutlined />}
-                >
-                  选择文件夹
-                </Button>
-              }
-            />
-            <ProFormSwitch label="下载完成提示" name="tip" />
-            <ProFormText
-              width="xl"
-              name="proxy"
-              placeholder="请填写代理地址"
-              label="代理设置"
-              fieldProps={{
-                addonAfter: (
-                  <Tooltip title="该代理会对软件自带浏览器以及下载时生效">
-                    <Switch
-                      checked={proxyChecked}
-                      checkedChildren="代理生效"
-                      unCheckedChildren="代理关闭"
-                      onChange={this.toggleProxySetting}
-                    />
-                  </Tooltip>
-                ),
-              }}
-            />
-          </ProFormGroup>
-          <ProFormGroup label="下载设置">
-            <ProFormSelect
-              allowClear={false}
-              width="xl"
-              name="exeFile"
-              label="默认下载器"
-              placeholder="请选择执行程序"
-              options={downloaderOptions}
-            />
-          </ProFormGroup>
-        </ProForm>
-        <Descriptions title={downloader.title}>
-          <Descriptions.Item label="源代码地址">
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                window.electron.openExternal(downloader.github);
-              }}
-            >
-              {downloader.github}
-            </a>
-          </Descriptions.Item>
-        </Descriptions>
-        <Space>
-          <Button onClick={this.openConfigDir} icon={<FolderOpenOutlined />}>
-            配置文件目录
-          </Button>
-          <Button onClick={this.openBinDir} icon={<FolderOpenOutlined />}>
-            可执行程序目录
-          </Button>
-          <Button onClick={this.localDir} icon={<FolderOpenOutlined />}>
-            本地存储目录
-          </Button>
-        </Space>
-      </div>
-    );
-  }
-}
-const mapStateToProps = (state: AppState) => ({
-  settings: state.settings.settings,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    setSettings: (settings: Partial<Settings>) =>
-      dispatch(updateSettings(settings)),
-  };
+          />
+          <ProFormSwitch label="下载完成提示" name="tip" />
+          <ProFormText
+            width="xl"
+            name="proxy"
+            placeholder="请填写代理地址"
+            label="代理设置"
+            fieldProps={{
+              addonAfter: (
+                <Tooltip title="该代理会对软件自带浏览器以及下载时生效">
+                  <Switch
+                    checked={proxyChecked}
+                    checkedChildren="代理生效"
+                    unCheckedChildren="代理关闭"
+                    onChange={toggleProxySetting}
+                  />
+                </Tooltip>
+              ),
+            }}
+          />
+        </ProFormGroup>
+        <ProFormGroup label="下载设置">
+          <ProFormSelect
+            allowClear={false}
+            width="xl"
+            name="exeFile"
+            label="默认下载器"
+            placeholder="请选择执行程序"
+            options={downloaderOptions}
+          />
+          <ProForm.Item label={"更多操作"}>
+            <Space>
+              <Button onClick={openConfigDir} icon={<FolderOpenOutlined />}>
+                配置文件目录
+              </Button>
+              <Button onClick={openBinDir} icon={<FolderOpenOutlined />}>
+                可执行程序目录
+              </Button>
+              <Button onClick={localDir} icon={<FolderOpenOutlined />}>
+                本地存储目录
+              </Button>
+            </Space>
+          </ProForm.Item>
+        </ProFormGroup>
+      </ProForm>
+    </Box>
+  );
 };
 
-const connector = connect(mapStateToProps, mapDispatchToProps);
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-export default connector(Setting);
+export default Setting;
