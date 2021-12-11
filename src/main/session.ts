6e1a90b6dd119f4d4cb845d0214b0a7218f0b27b
caorushizi <84996057@qq.com>
2021-12-11 15:18:56 +0800

    fix: electron 16.

--- src/main/session.ts
+++ src/main/session.ts
@@ -5,7 +5,7 @@ export default function createSession(partition: string): Electron.Session {
   const ses = session.fromPartition(partition);
 
   ses.protocol.registerFileProtocol("webview", (request, callback) => {
-    const url = request.url.substr(10);
+    const url = request.url.slice(0, 10);
     callback({ path: path.normalize(`${__dirname}/${url}`) });
   });
 
