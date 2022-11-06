0f0687b5c9edabec0461721178f2779761e408bb
士子☀️ <84996057@qq.com>
2022-11-06 23:36:28 +0800

    dev

--- packages/app/src/services/coreApp.ts
+++ packages/app/src/services/coreApp.ts
@@ -1,22 +1,19 @@
 import { inject, injectable } from 'inversify'
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, ipcMain } from 'electron'
 import { TYPES } from '../types'
 import { Config, DB, MainWindow, MyApp } from '../interfaces'
 
 @injectable()
 export class CoreApp implements MyApp {
-  private readonly app: Electron.App
   constructor (
     @inject(TYPES.DB) private readonly db: DB,
     @inject(TYPES.MainWindow) private readonly mainWindow: MainWindow,
     @inject(TYPES.Config) private readonly config: Config
   ) {
-    this.app = app
-
-    this.app.on('window-all-closed', () => {
+    app.on('window-all-closed', () => {
       if (process.platform !== 'darwin') app.quit()
     })
-    this.app.on('activate', () => {
+    app.on('activate', () => {
       if (BrowserWindow.getAllWindows().length === 0) {
         this.mainWindow.init().then(r => r).catch(e => e)
       }
@@ -26,9 +23,13 @@ export class CoreApp implements MyApp {
   async init (): Promise<void> {
     await Promise.all([
       this.db.init(),
-      this.app.whenReady()
+      app.whenReady()
     ])
     this.config.init()
     await this.mainWindow.init()
+
+    ipcMain.handle('my-invokable-ipc', async (event, path: string, data: any) => {
+
+    })
   }
 }
