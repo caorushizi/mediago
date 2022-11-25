7c3713612e2757cf0068c2ddf76ed5364796029d
士子☀️ <84996057@qq.com>
2022-11-26 00:15:05 +0800

    dev

--- packages/app/gulpfile.ts
+++ packages/app/gulpfile.ts
@@ -1,68 +1,69 @@
-import { series, dest, watch } from 'gulp'
-import { createProject } from 'gulp-typescript'
-import { join } from 'path'
-import { spawn, ChildProcessWithoutNullStreams } from 'child_process'
-import electron from 'electron'
-import fs from 'fs-extra'
+import { series, dest, watch } from "gulp";
+import { createProject } from "gulp-typescript";
+import { join } from "path";
+import { spawn, ChildProcessWithoutNullStreams } from "child_process";
+import fs from "fs-extra";
 
-const tsProject = createProject('tsconfig.json')
+const electron = join(__dirname, "node_modules/.bin/electron");
 
-let electronProcess: ChildProcessWithoutNullStreams | null = null
-let manualRestart = false
+const tsProject = createProject("tsconfig.json");
 
-function build (): NodeJS.WritableStream {
-  return tsProject.src().pipe(tsProject()).js.pipe(dest('dist'))
+let electronProcess: ChildProcessWithoutNullStreams | null = null;
+let manualRestart = false;
+
+function build(): NodeJS.WritableStream {
+  return tsProject.src().pipe(tsProject()).js.pipe(dest("dist"));
 }
 
-async function clean (): Promise<void> {
-  console.log('123123123')
-  const exist = await fs.pathExists('dist')
+async function clean(): Promise<void> {
+  console.log("123123123");
+  const exist = await fs.pathExists("dist");
   if (exist === true) {
-    await fs.rm('dist', { recursive: true })
-  }
+    await fs.rm("dist", { recursive: true });
+  } 
 }
 
-const dev = series(clean, build, startElectron)
-const restart = series(clean, build, restartElectron)
+const dev = series(clean, build, startElectron);
+const restart = series(clean, build, restartElectron);
 
-watch(['src/**'], restart)
+watch(["src/**"], restart);
 
-function restartElectron (): void {
-  console.log('watch build succeed.')
-  if ((electronProcess?.pid) != null) {
-    manualRestart = true
-    process.kill(electronProcess.pid)
-    electronProcess = null
-    startElectron()
+function restartElectron(): void {
+  console.log("watch build succeed.");
+  if (electronProcess?.pid != null) {
+    manualRestart = true;
+    process.kill(electronProcess.pid);
+    electronProcess = null;
+    startElectron();
 
     setTimeout(() => {
-      manualRestart = false
-    }, 5000)
+      manualRestart = false;
+    }, 5000);
   }
 }
 
-function startElectron (): void {
-  const args = ['--inspect=5858', join(__dirname, './dist/main.js')]
-  electronProcess = spawn(String(electron), args)
+function startElectron(): void {
+  const args = ["--inspect=5858", join(__dirname, "./dist/main.js")];
+  electronProcess = spawn(String(electron), args);
 
-  electronProcess.stdout.on('data', electronLog)
+  electronProcess.stdout.on("data", electronLog);
 
-  electronProcess.stderr.on('data', electronLog)
+  electronProcess.stderr.on("data", electronLog);
 
-  electronProcess.on('close', () => {
-    if (!manualRestart) process.exit()
-  })
+  electronProcess.on("close", () => {
+    if (!manualRestart) process.exit();
+  });
 }
 
-function electronLog (data: Buffer): void {
-  let log: string = ''
-  const lineStr = data.toString().split(/\r?\n/)
+function electronLog(data: Buffer): void {
+  let log: string = "";
+  const lineStr = data.toString().split(/\r?\n/);
   lineStr.forEach((line) => {
-    if (line.trim() !== '') {
-      log += `${line}\n`
+    if (line.trim() !== "") {
+      log += `${line}\n`;
     }
-  })
-  console.log(log)
+  });
+  console.log(log);
 }
 
-export { dev }
+export { dev };
