006cb4b13cea019bd4b59c4c8d4f18859eb3d930
士子☀️ <84996057@qq.com>
2022-12-10 19:14:22 +0800

    master

--- packages/main/src/preload.ts
+++ packages/main/src/preload.ts
@@ -1,21 +1,23 @@
 import { contextBridge, dialog, ipcRenderer, shell } from "electron";
 import { resolve } from "path";
 
+const { send, invoke, on, removeListener } = ipcRenderer;
+
 const apiKey = "electron";
 const api: ElectronApi = {
   store: {
     set(key, value) {
-      return ipcRenderer.invoke("set-store", key, value);
+      return invoke("set-store", key, value);
     },
     get(key) {
-      return ipcRenderer.invoke("get-store", key);
+      return invoke("get-store", key);
     },
   },
   isWindows: process.platform === "win32",
   isMacos: process.platform === "darwin",
-  ipcExec: (exeFile, args) => ipcRenderer.invoke("exec-command", exeFile, args),
+  ipcExec: (exeFile, args) => invoke("exec-command", exeFile, args),
   openBinDir: async () => {
-    const binDir = await ipcRenderer.invoke("get-bin-dir");
+    const binDir = await invoke("get-bin-dir");
     await shell.openPath(binDir);
   },
   openPath: (workspace) => shell.openPath(workspace),
@@ -24,29 +26,24 @@ const api: ElectronApi = {
       process.env.NODE_ENV === "development"
         ? "media downloader dev"
         : "media downloader";
-    const appPath = await ipcRenderer.invoke("get-path", "appData");
+    const appPath = await invoke("get-path", "appData");
     await shell.openPath(resolve(appPath, appName));
   },
   openExternal: (url, options) => shell.openExternal(url, options),
-  openBrowserWindow: (url) => ipcRenderer.send("open-browser-window", url),
-  closeBrowserWindow: () => ipcRenderer.send("close-browser-window"),
-  getPath: (name) => ipcRenderer.invoke("get-path", name),
-  showOpenDialog: (options) => {
-    return dialog.showOpenDialog(options);
-  },
-  getBrowserView: () => ipcRenderer.invoke("get-current-window"),
-  addEventListener: (channel, listener) => ipcRenderer.on(channel, listener),
-  removeEventListener: (channel, listener) =>
-    ipcRenderer.removeListener(channel, listener),
-  setBrowserViewRect: (rect) =>
-    ipcRenderer.send("set-browser-view-bounds", rect),
-  closeMainWindow: () => ipcRenderer.send("close-main-window"),
-  browserViewGoBack: () => ipcRenderer.send("browser-view-go-back"),
-  browserViewReload: () => ipcRenderer.send("browser-view-reload"),
-  browserViewLoadURL: (url) => ipcRenderer.send("browser-view-load-url", url),
-  itemContextMenu: (item) =>
-    ipcRenderer.send("open-download-item-context-menu", item),
-  minimize: (name) => ipcRenderer.send("window-minimize", name),
+  openBrowserWindow: (url) => send("open-browser-window", url),
+  closeBrowserWindow: () => send("close-browser-window"),
+  getPath: (name) => invoke("get-path", name),
+  showOpenDialog: (options) => dialog.showOpenDialog(options),
+  getBrowserView: () => invoke("get-current-window"),
+  addEventListener: (channel, listener) => on(channel, listener),
+  removeEventListener: (channel, listener) => removeListener(channel, listener),
+  setBrowserViewRect: (rect) => send("set-browser-view-bounds", rect),
+  closeMainWindow: () => send("close-main-window"),
+  browserViewGoBack: () => send("browser-view-go-back"),
+  browserViewReload: () => send("browser-view-reload"),
+  browserViewLoadURL: (url) => send("browser-view-load-url", url),
+  itemContextMenu: (item) => send("open-download-item-context-menu", item),
+  minimize: (name) => send("window-minimize", name),
 };
 
 contextBridge.exposeInMainWorld(apiKey, api);
