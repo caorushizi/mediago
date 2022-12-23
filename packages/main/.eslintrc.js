f702f847e6ec9d3424b294f368abdc9bfdc19930
士子☀️ <84996057@qq.com>
2022-12-23 22:02:11 +0800

    style: 修正代码提交规范

--- packages/main/.eslintrc.js
+++ packages/main/.eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react"],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "react-app",
-    "plugin:prettier/recommended",
-  ],
-  rules: {
-    "@typescript-eslint/ban-ts-comment": "warn",
-    "prettier/prettier": "error",
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "standard-with-typescript",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
--- packages/views/.eslintrc.js
+++ packages/views/.eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react"],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "react-app",
-    "plugin:prettier/recommended",
-  ],
-  rules: {
-    "@typescript-eslint/ban-ts-comment": "warn",
-    "prettier/prettier": "error",
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "standard-with-typescript"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
