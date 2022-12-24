1455ef417f50c9cbf97c85614206c6cb021708e3
士子☀️ <84996057@qq.com>
2022-12-24 17:51:28 +0800

    style: eslint 修正

--- packages/main/.eslintrc.js
+++ packages/main/.eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "standard-with-typescript",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "standard-with-typescript",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
