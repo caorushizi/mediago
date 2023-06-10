f085bcacf7cee8a8cad5ee82d34909c416562215
士子☀️ <84996057@qq.com>
2023-06-11 02:29:53 +0800

    add mobile

--- packages/mobile/src/views/HomeView.vue
+++ packages/mobile/src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+import Player from 'xgplayer'
+import 'xgplayer/dist/index.min.css'
+
+const videoRef = ref(null)
+
+onMounted(() => {
+  const player = new Player({
+    el: videoRef.value,
+    url: 'http://h5player.bytedance.com/video/mp4/xgplayer-demo-360p.mp4',
+    fluid: true
+  })
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div ref="videoRef"></div>
+
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell v-for="item in list" :key="item" :title="item" />
+    </van-list>
   </main>
 </template>
