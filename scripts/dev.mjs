926249184b3fa2ddef5b14de3c9efab7c113f4ee
士子☀️ <84996057@qq.com>
2024-02-08 15:12:08 +0800

    i18n

--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -1,12 +1,30 @@
 #!/usr/bin/env zx
 
+const platform = os.platform();
+
 echo("开始构建 development ...");
 echo("当前所在的目录是:", process.cwd());
 
-await $`npm run build:server`;
+const root = path.resolve(__dirname, "..");
+const main = path.resolve(root, "packages/main");
+const app = path.resolve(main, "app");
+const bin = path.resolve(app, "bin");
 
-await $`npm run build:mobile`;
+let filename = path.resolve(bin, `${platform}/server`)
+if (platform == "win32") {
+  filename += ".exe";
+}
 
-await $`npm run build:plugin`;
+if (!fs.existsSync(filename)) {
+  await $`npm run build:server`;
+}
+
+if (!fs.existsSync(path.resolve(app, 'mobile'))) {
+  await $`npm run build:mobile`;
+}
+
+if (!fs.existsSync(path.resolve(app, 'plugin'))) {
+  await $`npm run build:plugin`;
+}
 
 await $`npm run types`;
