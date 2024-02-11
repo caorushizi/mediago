8455f1751769405d88c477ff60743342de8f27c5
士子☀️ <84996057@qq.com>
2024-02-11 10:45:32 +0800

    style: 💄  somthing

--- packages/plugin/src/main.ts
+++ packages/plugin/src/main.ts
@@ -1,5 +1,9 @@
+// import "@spectrum-web-components/button/sp-button.js";
+// import "@spectrum-web-components/button/sp-clear-button.js";
+// import "@spectrum-web-components/button/sp-close-button.js";
+// import "@spectrum-web-components/alert-dialog/sp-alert-dialog.js";
+// import "@spectrum-web-components/dialog/sp-dialog-base.js";
 import "./components";
-import "./App";
 
 function bilibili() {
   const videoCards = document.querySelectorAll(
@@ -7,13 +11,12 @@ function bilibili() {
   );
   videoCards.forEach((card) => {
     const banner = document.createElement("one-button");
-    banner.name = "bili";
     card.appendChild(banner);
   });
 }
 
 function mount() {
-  const app = document.createElement("mediago-app");
+  const app = document.createElement("float-button");
   document.body.appendChild(app);
 }
 
