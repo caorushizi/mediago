da69002836dec21595c0657f5a38d1bae0abdbbb
士子☀️ <84996057@qq.com>
2024-02-25 22:37:34 +0800

    fix: 🐛  build player

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 
-import { echo,$ } from "zx";
+import { echo, $ } from "zx";
 
 echo("开始构建 production ...");
 echo("当前所在的目录是:", process.cwd());
@@ -11,7 +11,7 @@ await $`npm run build:server`;
 
 await $`npm run build:plugin`;
 
-await $`npm run build:mobile`;
+await $`npm run build:player`;
 
 await $`npm run build:main`;
 
