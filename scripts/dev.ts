bac30577caa5a7fb177db8a292a5ed783a827620
caorushizi <84996057@qq.com>
2024-03-20 19:29:27 +0800

    refactor: 📦️  remove tsno

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,25 +1,16 @@
 #!/usr/bin/env zx
-
-import { os, echo, path, fs, $ } from "zx";
-
-const platform = os.platform();
+import { fileURLToPath } from 'url';
+import { echo, path, fs, $ } from "zx";
 
 echo("开始构建 development ...");
 echo("当前所在的目录是:", process.cwd());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const root = path.resolve(__dirname, "..");
 const main = path.resolve(root, "packages/main");
 const app = path.resolve(main, "app");
-const bin = path.resolve(app, "bin");
-
-let filename = path.resolve(bin, `${platform}/server`);
-if (platform == "win32") {
-  filename += ".exe";
-}
-
-if (!fs.existsSync(filename)) {
-  await $`npm run build:server`;
-}
 
 if (!fs.existsSync(path.resolve(app, "plugin"))) {
   await $`npm run build:plugin`;
