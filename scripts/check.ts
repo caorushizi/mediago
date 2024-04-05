45cde40cea81f0ddac5436eb16d9a8823dffe00b
士子☀️ <84996057@qq.com>
2024-04-05 22:29:45 +0800

    chore: 🔨  fix pre-commit

--- scripts/check.ts
+++ scripts/check.ts
@@ -1,19 +1,20 @@
 #!/usr/bin/env zx
 
-import { echo } from "zx";
+import { $, echo } from "zx";
+import type { ProcessOutput } from "zx";
+import { printObject } from "./utils";
 
 echo("开始执行代码质量评估...\n");
-// import type { ProcessOutput } from "zx";
-// import { printObject } from "./utils";
 
-// await $`pnpm spellcheck`.catch((out: ProcessOutput) => {
-//   throw new Error(out.stdout);
-// });
+await $`pnpm spellcheck`.catch((out: ProcessOutput) => {
+  printObject(out);
+  throw new Error(out.stdout);
+});
 
 // check type and stage
-// await Promise.all([$`pnpm types`, $`pnpm lint-stage`]).catch(
-//   (out: ProcessOutput) => {
-//     printObject(out);
-//     throw new Error(out.stdout);
-//   },
-// );
+await Promise.all([$`pnpm types`, $`pnpm lint-staged`]).catch(
+  (out: ProcessOutput) => {
+    printObject(out);
+    throw new Error(out.stdout);
+  },
+);
