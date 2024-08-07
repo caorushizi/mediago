29602d9fa2b2f65e7abd32a7faa8126e466bb618
fenglei <wfl12036@163.com>
2024-08-07 18:37:23 +0800

    docs: update icon button (#243)

--- packages/renderer/src/components/IconButton.tsx
+++ packages/renderer/src/components/IconButton.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: fenglei wfl12036@163.com
+ * @Date: 2024-08-07 18:23:03
+ * @LastEditors: fenglei wfl12036@163.com
+ * @LastEditTime: 2024-08-07 18:32:52
+ * @FilePath: /mediago/packages/renderer/src/components/IconButton.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { cn } from "@/utils";
 import { useMemoizedFn } from "ahooks";
 import React, { cloneElement, PropsWithChildren, ReactElement } from "react";
@@ -10,6 +18,7 @@ interface Props extends PropsWithChildren {
   highlight?: boolean;
 }
 
+// IconButton
 export function IconButton({
   children,
   disabled,
