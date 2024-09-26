03ab5ef55bda48df632d2e1ec792de5ab647d95e
caorushizi <84996057@qq.com>
2024-09-26 21:51:09 +0800

    feat: ✨  optimized app ICONS

--- packages/mobile/src/utils.ts
+++ packages/mobile/src/utils.ts
@@ -1,6 +1,13 @@
 import { twMerge } from "tailwind-merge";
 import { type ClassValue, clsx } from "clsx";
+import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+console.log(import.meta.env.MODE);
+export const http = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development" ? "http://localhost:3222" : "/",
+});
