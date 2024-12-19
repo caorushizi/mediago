df06736722a4ab4701499de0791d762bf4b60536
caorushizi <84996057@qq.com>
2024-12-19 20:39:31 +0800

    fix: 🐛  windows script

--- scripts/web.ts
+++ scripts/web.ts
@@ -2,6 +2,10 @@
 import { echo, $ } from "zx";
 
 $.verbose = true;
+if (process.platform === "win32") {
+  $.prefix = "";
+  $.shell = "pwsh.exe";
+}
 
 echo("开始构建 production ...");
 echo("当前所在的目录是:", process.cwd());
