4b78891c0af552d29dc670aeb3d9e0019bd69593
caorushizi <84996057@qq.com>
2025-10-23 13:25:01 +0800

    feat: use tsdown

---
+++ apps/electron/tsdown.config.ts
@@ -0,0 +1,60 @@
+import { type ChildProcessWithoutNullStreams, spawn } from "node:child_process";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import electron from "electron";
+import { defineConfig } from "tsdown";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export class ElectronApp {
+  process: ChildProcessWithoutNullStreams | null = null;
+
+  start() {
+    const args = ["--inspect=5858", path.resolve(__dirname, "./build/index.js")];
+
+    this.process = spawn(String(electron), args);
+
+    this.process.stdout.on("data", (data) => {
+      console.log(String(data));
+    });
+
+    this.process.stderr.on("data", (data) => {
+      console.log(String(data));
+    });
+  }
+
+  restart() {
+    this.kill();
+    this.start();
+  }
+
+  kill() {
+    if (this.process?.pid) {
+      if (process.platform === "win32") {
+        process.kill(this.process.pid);
+      } else {
+        spawn("kill", ["-9", String(this.process.pid)]);
+      }
+      this.process = null;
+    }
+  }
+}
+
+const app = new ElectronApp();
+
+export default defineConfig({
+  outDir: "build",
+  shims: true,
+  loader: {
+    ".jpg": "asset",
+    ".png": "asset",
+  },
+  hooks: {
+    "build:done": () => {
+      if (process.env.NODE_ENV === "development") {
+        app.restart();
+      }
+    },
+  },
+});
