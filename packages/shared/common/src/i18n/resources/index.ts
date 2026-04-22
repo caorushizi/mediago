1820ae2b4937693f0b129827e53e0d0ee1739e6b
bovirus <1262554+bovirus@users.noreply.github.com>
2026-04-22 20:44:10 +0200

    Add settings for Italian

--- packages/shared/common/src/i18n/resources/index.ts
+++ packages/shared/common/src/i18n/resources/index.ts
@@ -1,13 +1,15 @@
 import { en } from "./en";
 import { zh } from "./zh";
+import { it } from "./it";
 
 export const i18nResources = {
   en,
   zh,
+  it,
 } as const;
 
-export const SUPPORTED_LANGUAGES = ["en", "zh"] as const;
+export const SUPPORTED_LANGUAGES = ["en", "zh", "it"] as const;
 export const DEFAULT_BACKEND_NAMESPACE = "backend" as const;
 export const DEFAULT_FRONTEND_APP = "main" as const;
 
-export { en, zh };
+export { en, zh, it };
